573255edc9510a69edf9eedd7d9fcb53a7d7c394
Your Name <vijaynaren@outlook.com>
2026-02-26 20:12:21 +0530

    push check

--- src/components/HeroSection.js
+++ src/components/HeroSection.js
@@ -12,7 +12,7 @@ const HeroSection = () => {
       animate={{ opacity: 1 }}
       transition={{ duration: 1 }}
     >
-      Established in 2013 by Mr. Vadivel S. G., Vijay Tex began with 50 looms dedicated to producing a single, standard quality of 100% pure grey cotton fabric. Through the founder’s diligent leadership and hard work, we have scaled our operations to over 500 looms, enabling us to consistently deliver multiple premium qualities of 100% cotton fabric today. Our scale allows us to achieve a total weekly production capacity exceeding 250,000 meters across all our qualities.
+      Established in 2014 by Mr. Vadivel S. G., Vijay Tex began with 50 looms dedicated to producing a single, standard quality of 100% pure grey cotton fabric. Through the founder’s diligent leadership and hard work, we have scaled our operations to over 500 looms, enabling us to consistently deliver multiple premium qualities of 100% cotton fabric today. Our scale allows us to achieve a total weekly production capacity exceeding 250,000 meters across all our qualities.
     </motion.section>
   );
 };
